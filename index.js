73df510d17282765d0fb753e9b7c562a850abbf0
github username <github email address>
2021-04-06 00:14:48 -0500

    commit tree 0f7b8b4b7c2e010bc5d27c0de805cfbe8a96e8ef

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+var newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "Joe Mama is the champion";
+document.body.appendChild(newHeader);
